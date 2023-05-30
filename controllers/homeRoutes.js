61452e9a7170985587b98201765f1e751649b230
Teaom <dclarkjenkins87@gmail.com>
2023-05-30 16:11:28 -0600

    revert changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,19 +4,44 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
+    // Get all post and JOIN with user data
     const postData = await Post.findAll({
       include: [
         {
           model: User,
-          attributes: ['username']
+          attributes: ['username'],
         },
       ],
     });
 
     // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: false }));
-// res.json(posts)
+    const posts = postData.map((post) => post.get({ plain: true }));
+  // res.json(posts)
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/comments', async (req, res) => {
+  try {
+    // Get all post and JOIN with user data
+    const commentData = await Comment.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+      });
+
+    // Serialize data so the template can read it
+    const comments = commentData.map((post) => comment.get({ plain: false }));
+// res.json(comments)
     // Pass serialized data and session flag into template
     res.render('homepage', { 
       posts, 
@@ -34,15 +59,15 @@ router.get('/post/:id', async (req, res) => {
         User,
         {
           model: Comment,
-         include: [User],
+          include: [User],
         },
       ],
     });
 
-    const posts = postData.get({ plain: true });
-// res.json(posts)
-    res.render('singlepost', {
-      posts,
+    const post = postData.get({ plain: true });
+// res.json(post)
+    res.render('singlePost', {
+      post,
       logged_in: req.session.logged_in
     });
   } catch (err) {
@@ -50,32 +75,7 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
-router.get('/comments', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    const commentData = await Comment.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['username'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const comments = commentData.map((comment) => comment.get({ plain: false }));
-// res.json(comments)
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      posts, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// // Use withAuth middleware to prevent access to route
+// Use withAuth middleware to prevent access to route
 // router.get('/profile', withAuth, async (req, res) => {
 //   try {
 //     // Find the logged in user based on the session ID
@@ -116,13 +116,7 @@ router.get('/signup', (req, res) => {
 });
 
 router.get('/newpost', (req, res) => {
-
- 
   res.render('newpost');
 });
-
-// router.get('/editpost', (req, res) => {
-//   res.render('editpost');
-// });
 
 module.exports = router;
